2c116cc5b0ed4bd6d65ff602a8f49960428992b0
FADLI Saïd <saidfadli224@gmail.com>
2025-04-23 17:46:19 +0200

    date rdv

--- src/Controller/controller-date.php
+++ src/Controller/controller-date.php
@@ -4,6 +4,7 @@ session_start();
 
 include_once "../../config.php";
 
+
 $mois = [
     1 => "Janvier",
     2 => "Février",
@@ -19,6 +20,14 @@ $mois = [
     12 => "Décembre"
 ];
 
-$currentMonth = date("n");
+$jours = [
+    1 => "Lundi",
+    2 => "Mardi",
+    3 => "Mercredi",
+    4 => "Jeudi",
+    5 => "Vendredi",
+    6 => "Samedi",
+    7 => "Dimanche",
+];
 
-include_once "../View/view-reservation.php";
+include_once "../View/view-date.php";
--- src/View/view-reservation.php
+++ src/View/view-reservation.php
@@ -7,167 +7,103 @@
         include_once "../../templates/deco-nav.php";
     } ?>
 
-
     <section id="reservation">
         <a href="../Controller/controller-accueil.php" class="w-100 text-start retour"><i class="fas fa-arrow-left"></i>
             Accueil</a>
         <h1 class="text-center">Réservation</h1>
-        <div id="carouselExample" class="carousel slide">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <table class="container">
-                        <caption>
-                            <h3 class="text-center"><?= $mois[date("n")] ?></h3>
-                        </caption>
-                        <thead>
-                            <tr>
-                                <th>Lun</th>
-                                <th>Mar</th>
-                                <th>Mer</th>
-                                <th>Jeu</th>
-                                <th>Ven</th>
-                                <th>Sam</th>
-                                <th>Dim</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $Year = date("Y");
-                            $Month = date("n");
-                            $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $Month, $Year);
-                            $firstDay = mktime(0, 0, 0, $Month, 1, $Year);
-                            $firstDayOfWeek = date("N", $firstDay);
-                            $currentDay = 1;
-                            for ($i = 0; $i < 7; $i++) {
-                                echo "<tr>";
-                                if ($currentDay > $daysInMonth) {
-                                    break;
-                                }
-                                for ($j = 1; $j <= 7; $j++) {
-                                    if ($i == 0 && $j < $firstDayOfWeek) {
-                                        echo "<td></td>";
-                                    } elseif ($currentDay > $daysInMonth) {
-                                        echo "<td></td>";
-                                    } else {
-                                        echo "<td>$currentDay</td>";
-                                        $currentDay++;
-                                    }
-                                }
-                                echo "</tr>";
+        <div class="d-flex justify-content-center">
+            <button class="btn d-none" id="avant"><i class="fa-solid fa-chevron-left"></i></button>
+
+            <?php for ($offset = 0; $offset <= 2; $offset++): ?>
+                <table class="container m-0 <?= $offset > 0 ? 'd-none' : '' ?>" id="mois<?= $offset ?>">
+                    <caption>
+                        <h2 class="text-center">
+                            <?= $mois[$currentMonth + $offset] ?>
+                        </h2>
+                    </caption>
+                    <thead>
+                        <tr>
+                            <th>Lun</th>
+                            <th>Mar</th>
+                            <th>Mer</th>
+                            <th>Jeu</th>
+                            <th>Ven</th>
+                            <th>Sam</th>
+                            <th>Dim</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $Year = date("Y");
+                        $Month = $currentMonth + $offset;
+                        $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $Month, $Year);
+                        $firstDay = mktime(0, 0, 0, $Month, 1, $Year);
+                        $firstDayOfWeek = date("N", $firstDay);
+                        $currentDay = 1;
+                        for ($i = 0; $i < 7; $i++) {
+                            echo "<tr>";
+                            if ($currentDay > $daysInMonth) {
+                                break;
                             }
-                            ?>
-                        </tbody>
-                    </table>
-                </div>
-
-
-                <div class="carousel-item">
-                    <table class="container">
-                        <caption>
-                            <h3 class="text-center"><?= $mois[date("n") + 1] ?></h3>
-                        </caption>
-                        <thead>
-                            <tr>
-                                <th>Lun</th>
-                                <th>Mar</th>
-                                <th>Mer</th>
-                                <th>Jeu</th>
-                                <th>Ven</th>
-                                <th>Sam</th>
-                                <th>Dim</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $Year = date("Y");
-                            $Month = date("n") + 1;
-                            $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $Month, $Year);
-                            $firstDay = mktime(0, 0, 0, $Month, 1, $Year);
-                            $firstDayOfWeek = date("N", $firstDay);
-                            $currentDay = 1;
-                            for ($i = 0; $i < 7; $i++) {
-                                echo "<tr>";
-                                if ($currentDay > $daysInMonth) {
-                                    break;
+                            for ($j = 1; $j <= 7; $j++) {
+                                if ($i == 0 && $j < $firstDayOfWeek) {
+                                    echo "<td></td>";
+                                } elseif ($currentDay > $daysInMonth) {
+                                    echo "<td></td>";
+                                } else {
+                                    $dayOfWeek = date("N", mktime(0, 0, 0, $Month, $currentDay, $Year));
+                                    echo "<td><a href='controller-date.php?month=" . $Month . "&numberDay=" . $currentDay . "&day=" . $dayOfWeek . "'>$currentDay</a></td>";
+                                    $currentDay++;
                                 }
-                                for ($j = 1; $j <= 7; $j++) {
-                                    if ($i == 0 && $j < $firstDayOfWeek) {
-                                        echo "<td></td>";
-                                    } elseif ($currentDay > $daysInMonth) {
-                                        echo "<td></td>";
-                                    } else {
-                                        echo "<td>$currentDay</td>";
-                                        $currentDay++;
-                                    }
-                                }
-                                echo "</tr>";
                             }
-                            ?>
-                        </tbody>
-                    </table>
-                </div>
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            <?php endfor; ?>
 
-
-                <div class="carousel-item">
-                    <table class="container">
-                        <caption>
-                            <h3 class="text-center"><?= $mois[date("n") + 2] ?></h3>
-                        </caption>
-                        <thead>
-                            <tr>
-                                <th>Lun</th>
-                                <th>Mar</th>
-                                <th>Mer</th>
-                                <th>Jeu</th>
-                                <th>Ven</th>
-                                <th>Sam</th>
-                                <th>Dim</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            $Year = date("Y");
-                            $Month = date("n") + 2;
-                            $daysInMonth = cal_days_in_month(CAL_GREGORIAN, $Month, $Year);
-                            $firstDay = mktime(0, 0, 0, $Month, 1, $Year);
-                            $firstDayOfWeek = date("N", $firstDay);
-                            $currentDay = 1;
-                            for ($i = 0; $i < 7; $i++) {
-                                echo "<tr>";
-                                if ($currentDay > $daysInMonth) {
-                                    break;
-                                }
-                                for ($j = 1; $j <= 7; $j++) {
-                                    if ($i == 0 && $j < $firstDayOfWeek) {
-                                        echo "<td></td>";
-                                    } elseif ($currentDay > $daysInMonth) {
-                                        echo "<td></td>";
-                                    } else {
-                                        echo "<td>$currentDay</td>";
-                                        $currentDay++;
-                                    }
-                                }
-                                echo "</tr>";
-                            }
-                            ?>
-                        </tbody>
-                    </table>
-                </div>
-
-
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+            <button class="btn" id="apres"><i class="fa-solid fa-chevron-right"></i></button>
         </div>
     </section>
 
     <?php include_once "../../templates/footer.php" ?>
+    <script>
+        const avant = document.querySelector("#avant");
+        const apres = document.querySelector("#apres");
+        const currentMonth = <?= $currentMonth ?>;
+        let newMonth = currentMonth;
+
+        avant.addEventListener("click", function() {
+            newMonth--;
+            updateVisibility();
+        });
+
+        apres.addEventListener("click", function() {
+            newMonth++;
+            updateVisibility();
+        });
+
+        function updateVisibility() {
+            for (let i = 0; i <= 2; i++) {
+                document.getElementById(`mois${i}`).classList.add("d-none");
+            }
+
+            if (newMonth === currentMonth) {
+                avant.classList.add("d-none");
+            } else {
+                avant.classList.remove("d-none");
+            }
+
+            if (newMonth === currentMonth + 2) {
+                apres.classList.add("d-none");
+            } else {
+                apres.classList.remove("d-none");
+            }
+
+            document.getElementById(`mois${newMonth - currentMonth}`).classList.remove("d-none");
+        }
+    </script>
 </body>
 
 </html>
